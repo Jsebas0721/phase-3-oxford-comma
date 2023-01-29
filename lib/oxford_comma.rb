2862e3e7d3523815d268b11380300b5c3b701d84
sebastian medina <sebastianlopez_21@hotmail.com>
2023-01-29 16:30:26 -0500

    commit tree 101b78e8e218df7358b925e66c70df5ff3effa58

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+    except_last = []
+    if array.length > 2
+         except_last << array.slice(0,array.length - 1)
+         return except_last.join(", ") <<  ", and #{array[-1]}"
+    elsif array.length == 1
+        return array[0]
+    elsif array.length == 2
+        return array.join(" and ")
 
+    end
+
+        
+        
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
